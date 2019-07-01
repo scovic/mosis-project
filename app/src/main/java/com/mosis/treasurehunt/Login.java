57d24462d000c99816bac2062d369b055b8a98a2
Stefan Čović <scovic996@gmail.com>
2019-07-01 22:00:35 +0200

    Add Login and Register UI

---
+++ app/src/main/java/com/mosis/treasurehunt/Login.java
@@ -0,0 +1,30 @@
+package com.mosis.treasurehunt;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
+
+        Button noAccountButton = (Button) findViewById(R.id.button_no_account);
+        noAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Login.this, Register.class);
+                startActivity(i);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/mosis/treasurehunt/Register.java
@@ -0,0 +1,29 @@
+package com.mosis.treasurehunt;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_register);
+
+        Button haveAccountButton = (Button)findViewById(R.id.button_have_account);
+        haveAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Register.this, Login.class);
+                startActivity(i);
+            }
+        });
+    }
+}
