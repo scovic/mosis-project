c1f48ac73c71fa308b68708ed06e3f2f8030edd8
nensiiika <mrsnevenac@gmail.com>
2019-07-04 20:25:53 +0200

    add userProfile

--- app/src/main/java/com/mosis/treasurehunt/UserProfileActivity.java
+++ app/src/main/java/com/mosis/treasurehunt/UserProfileActivity.java
@@ -1,21 +1,18 @@
 package com.mosis.treasurehunt;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
-public class Home extends AppCompatActivity {
+public class UserProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_user_profile);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -29,22 +26,4 @@ public class Home extends AppCompatActivity {
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.show_user_profile) {
-            Intent i = new Intent(this, UserProfileActivity.class);
-            startActivity(i);
-        }
-
-
-        return super.onOptionsItemSelected(item);
-    }
 }
