cf7cf3f7e750f2bad42332ab2002c5ec40d026ec
nensiiika <mrsnevenac@gmail.com>
2019-07-04 21:37:09 +0200

    add spinner

--- app/src/main/java/com/mosis/treasurehunt/UserProfileActivity.java
+++ app/src/main/java/com/mosis/treasurehunt/UserProfileActivity.java
@@ -6,11 +6,23 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class UserProfileActivity extends AppCompatActivity {
 
+    Spinner spinner;
+    ArrayAdapter<CharSequence> spinnerAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+//        spinner = findViewById(R.id.spinner_profile);
+//        spinnerAdapter = ArrayAdapter.createFromResource(this, R.array.type_of_hunts_array, android.R.layout.simple_spinner_item);
+//        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spinner.setAdapter(spinnerAdapter);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
         Toolbar toolbar = findViewById(R.id.toolbar);
